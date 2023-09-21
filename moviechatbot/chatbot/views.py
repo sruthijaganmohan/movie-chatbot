96ddd5b8c14bbb72e2a29da9b3334c8ad77b6f3c
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-09-21 19:28:35 +0530

    api integration

--- moviechatbot/chatbot/views.py
+++ moviechatbot/chatbot/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+# openai_api_key = 'key'
+openai_api_key = 'key'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=message,
+        max_tokens=150,
+        stop=None,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
 
 # Create your views here.
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message' : message, 'response': response})
+    return render(request, 'chatbot.html')
